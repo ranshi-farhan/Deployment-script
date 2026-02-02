a6404f6b5d8082530f93390df69b2fe100da2db1
ranshi-farhan <ranshifarhanusmanak@gmail.com>
2026-02-02 22:13:18 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
-# USE UBUNTU20.04 - INSTANCE: 2GB RAM + 2VCPU MIN - WILL ONLY WORK
-sudo apt update -y
-sudo apt install openjdk-17-jdk -y
-sudo apt update -y
-sudo apt install maven -y
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+set -e
+
+# Update system
+apt update -y
+
+# Install Java 17 (required by Jenkins)
+apt install -y openjdk-17-jdk
+
+# Add Jenkins key (NEW method)
+mkdir -p /usr/share/keyrings
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key \
+  | tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+# Add Jenkins repo
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt update -y
-sudo apt install jenkins -y
-service jenkins start
-cat /var/lib/jenkins/secrets/initialAdminPassword
-#chmod 777 jenkins.sh
-#./jenkins.sh
+https://pkg.jenkins.io/debian-stable binary/ \
+| tee /etc/apt/sources.list.d/jenkins.list
+
+# Update again
+apt update -y
+
+# Install Jenkins
+apt install -y jenkins
+
+# Start & enable Jenkins
+systemctl enable jenkins
+systemctl start jenkins
+
